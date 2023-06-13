cf3fae5dfcca4f5544ae2b42ba20ad6d12ae784d
Matty <mattheas@ualberta.ca>
2023-06-12 20:04:22 -0600

    Queue is added for inter process communication. The readSensorData task is now strictly responsible for reading sensor data and posting it to the queue, while the transmit sensor data, fetches data from the queue and transmits it back to the PC via Serial communication. There are also initilization checks added that communicate if task and/ or queue initialization was unsuccessful

--- data_logger.ino
+++ data_logger.ino
@@ -1,4 +1,5 @@
 #include <Arduino_FreeRTOS.h>
+#include <queue.h>
 
 #define sensorPowerPin 52
 #define sensorOutputPin A0
@@ -6,6 +7,7 @@
 void TaskReadSensorData( void *pvParameters );
 void TaskTransmitSensorData( void *pvParameters );
 
+QueueHandle_t xQueue_FIFO = NULL;	//queue between two tasks 
 
 void setup() 
 { 
@@ -17,24 +19,33 @@ void setup()
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
 
-  // Create two tasks
-  xTaskCreate(    TaskReadSensorData                /* The function that implements the task. */
-                  , "ReadSensorData"                /* Text name for the task, provided to assist debugging only. */
-                  , configMINIMAL_STACK_SIZE        /* The stack allocated to the task. */
-                  , NULL                            /* The task parameter is not used, so set to NULL. */
-                  , 2                               /* The priority at which the task is created */
-                  , NULL );                         /* Used to pass out the created task's handle. It is not used so set to NULL */
+  // Create two tasks, one for reading sensor data, and one for transmitting it back to PC
+  BaseType_t Task1 = xTaskCreate(    TaskReadSensorData                 /* The function that implements the task. */
+                                      , "ReadSensorData"                /* Text name for the task, provided to assist debugging only. */
+                                      , configMINIMAL_STACK_SIZE        /* The stack allocated to the task. */
+                                      , NULL                            /* The task parameter is not used, so set to NULL. */
+                                      , 2                               /* The priority at which the task is created */
+                                      , NULL );                         /* Used to pass out the created task's handle. It is not used so set to NULL */
   
-  xTaskCreate(    TaskTransmitSensorData                /* The function that implements the task. */
-                  , "TransmitSensorData"                /* Text name for the task, provided to assist debugging only. */
-                  , configMINIMAL_STACK_SIZE        /* The stack allocated to the task. */
-                  , NULL                            /* The task parameter is not used, so set to NULL. */
-                  , 1                               /* The priority at which the task is created */
-                  , NULL );                         /* Used to pass out the created task's handle. It is not used so set to NULL */
+  BaseType_t Task2 = xTaskCreate(    TaskTransmitSensorData            /* The function that implements the task. */
+                                      , "TransmitSensorData"            /* Text name for the task, provided to assist debugging only. */
+                                      , configMINIMAL_STACK_SIZE        /* The stack allocated to the task. */
+                                      , NULL                            /* The task parameter is not used, so set to NULL. */
+                                      , 1                               /* The priority at which the task is created */
+                                      , NULL );                         /* Used to pass out the created task's handle. It is not used so set to NULL */
   
+  // Create Queue for inter-process communication
+  xQueue_FIFO = xQueueCreate( 10, sizeof( int ) ); //connects task1 -> task2
+
+  // Check that Tasks and Queue were initialized properly
+  if (xQueue_FIFO == NULL) { Serial.println("QUEUE WAS NOT INITIALIZED"); }
+  if (Task1 != pdPASS || Task2 != pdPASS) { Serial.println("TASK(S) WERE NOT INITIALIZED"); }
+
+
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
 
   Serial.println("Setup() complete");
+
 }
 
 
@@ -53,10 +64,11 @@ void TaskReadSensorData(void *pvParameters) // This is a task.
 {
   (void) pvParameters;
 
-  for(;;)
+  for (;;)
   {
     int sensorValue = analogRead(sensorOutputPin);
-    Serial.println(sensorValue);
+    //Serial.println(sensorValue);
+    xQueueSendToBack(xQueue_FIFO, &sensorValue, 0UL);
     vTaskDelay(1);
 
   }
@@ -68,8 +80,12 @@ void TaskTransmitSensorData(void *pvParameters) // This is a task.
 {
   (void) pvParameters;
 
-  for(;;){
-    Serial.println("Transmit sensor data task");
+  int recievedData;
+
+  for (;;)
+  {
+    xQueueReceive(xQueue_FIFO, &recievedData, portMAX_DELAY); // Waits indefinetely for the queue to have data
+    Serial.println(recievedData);
     vTaskDelay(1);
 
   }
