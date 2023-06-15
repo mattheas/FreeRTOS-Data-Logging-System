5bab52ab0e715f462f3969c7b4b36f14205749f5
Matty <mattheas@ualberta.ca>
2023-06-14 23:15:36 -0600

    Arduino script is updated, both Tasks now have a safe stack size (minSize*2), and also the same priority (2). With the two tasks now at the same priorty, they should act in a round-robin time slicing manner. I also updated the vTaskDelay() of TaskReadSensorData, so now the sensor is polled every 5000ms (5 seconds). Also some comments/ spacing are updated

--- data_logger.ino
+++ data_logger.ino
@@ -6,14 +6,13 @@
 
 void TaskReadSensorData( void *pvParameters );
 void TaskTransmitSensorData( void *pvParameters );
-
-QueueHandle_t xQueue_FIFO = NULL;	//queue between two tasks 
+QueueHandle_t xQueue_FIFO = NULL;	//queue for inter-task communication 
 
 void setup() 
 { 
+  // initialize digital and analog pins
   pinMode(sensorOutputPin, INPUT);
   pinMode(sensorPowerPin, OUTPUT);
-
   digitalWrite(sensorPowerPin, HIGH);
 
   // initialize serial communication at 9600 bits per second:
@@ -22,16 +21,16 @@ void setup()
   // Create two tasks, one for reading sensor data, and one for transmitting it back to PC
   BaseType_t Task1 = xTaskCreate(    TaskReadSensorData                 /* The function that implements the task. */
                                       , "ReadSensorData"                /* Text name for the task, provided to assist debugging only. */
-                                      , configMINIMAL_STACK_SIZE        /* The stack allocated to the task. */
+                                      , configMINIMAL_STACK_SIZE*2      /* The stack allocated to the task. */
                                       , NULL                            /* The task parameter is not used, so set to NULL. */
                                       , 2                               /* The priority at which the task is created */
                                       , NULL );                         /* Used to pass out the created task's handle. It is not used so set to NULL */
   
-  BaseType_t Task2 = xTaskCreate(    TaskTransmitSensorData            /* The function that implements the task. */
+  BaseType_t Task2 = xTaskCreate(    TaskTransmitSensorData             /* The function that implements the task. */
                                       , "TransmitSensorData"            /* Text name for the task, provided to assist debugging only. */
-                                      , configMINIMAL_STACK_SIZE        /* The stack allocated to the task. */
+                                      , configMINIMAL_STACK_SIZE*2      /* The stack allocated to the task. */
                                       , NULL                            /* The task parameter is not used, so set to NULL. */
-                                      , 1                               /* The priority at which the task is created */
+                                      , 2                               /* The priority at which the task is created */
                                       , NULL );                         /* Used to pass out the created task's handle. It is not used so set to NULL */
   
   // Create Queue for inter-process communication
@@ -41,10 +40,7 @@ void setup()
   if (xQueue_FIFO == NULL) { Serial.println("QUEUE WAS NOT INITIALIZED"); }
   if (Task1 != pdPASS || Task2 != pdPASS) { Serial.println("TASK(S) WERE NOT INITIALIZED"); }
 
-
   // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
-
-  Serial.println("Setup() complete");
 
 }
 
@@ -66,10 +62,12 @@ void TaskReadSensorData(void *pvParameters) // This is a task.
 
   for (;;)
   {
+    // Read sensor value and write it to queue
     int sensorValue = analogRead(sensorOutputPin);
-    //Serial.println(sensorValue);
     xQueueSendToBack(xQueue_FIFO, &sensorValue, 0UL);
-    vTaskDelay(1);
+
+    // block task for 5 seconds, i.e., makes it so that it polls the sensor every 5 seconds
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
 
   }
 
@@ -79,13 +77,14 @@ void TaskReadSensorData(void *pvParameters) // This is a task.
 void TaskTransmitSensorData(void *pvParameters) // This is a task.
 {
   (void) pvParameters;
-
   int recievedData;
 
   for (;;)
   {
+    // Read data from queue and send it over serial port
     xQueueReceive(xQueue_FIFO, &recievedData, portMAX_DELAY); // Waits indefinetely for the queue to have data
     Serial.println(recievedData);
+
     vTaskDelay(1);
 
   }
